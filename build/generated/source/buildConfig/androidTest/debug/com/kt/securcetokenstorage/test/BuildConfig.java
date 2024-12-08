abd7a567d2d17743af7279ca6e5cc14dd2491997
ngocanh <Ngocanh09082000@gmail.com>
2024-12-08 11:32:51 +0700

    release project

---
+++ build/generated/source/buildConfig/androidTest/debug/com/kt/securcetokenstorage/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.kt.securcetokenstorage.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.kt.securcetokenstorage.test";
+  public static final String BUILD_TYPE = "debug";
+}
---
+++ build/generated/source/buildConfig/debug/com/kt/securcetokenstorage/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.kt.securcetokenstorage;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.kt.securcetokenstorage";
+  public static final String BUILD_TYPE = "debug";
+}
